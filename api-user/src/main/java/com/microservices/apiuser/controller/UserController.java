6c005832815740e4a3a7efd4791ad4642f04c8aa
lutrapp <lutrapp@gmail.com>
2023-06-02 22:34:52 -0300

    implemented methods delete insertUser

--- api-user/src/main/java/com/microservices/apiuser/controller/UserController.java
+++ api-user/src/main/java/com/microservices/apiuser/controller/UserController.java
@@ -4,13 +4,20 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.apiuser.dto.UserDto;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
 
 //@RestController
 //@RequestMapping("/user")
@@ -46,9 +53,28 @@ public class UserController {
 		usuarios.add(userDto);
 		usuarios.add(userDto2);
 	}
-	
+
 	@GetMapping
-	public	List<UserDto>	getUsers() {
-					return	usuarios;
+	public List<UserDto> getUsers() {
+		return usuarios;
+	}
+
+	@GetMapping("/{cpf}")
+	public UserDto getUserByCpf(@PathVariable String cpf) {
+		return usuarios.stream().filter(userDto -> userDto.getCpf().equals(cpf)).findFirst()
+				.orElseThrow(() -> new RuntimeException("User not found"));
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public UserDto insertNewUser(@RequestBody @Valid UserDto userDto) {
+		userDto.setDataCadastro(LocalDateTime.now());
+		usuarios.add(userDto);
+		return userDto;
+	}
+	
+	@DeleteMapping("/{cpf}")
+	public boolean removeUser(@PathVariable String cpf) {
+		return usuarios.removeIf(userDto -> userDto.getCpf().equals(cpf));
 	}
 }
--- api-user/src/main/java/com/microservices/apiuser/dto/UserDto.java
+++ api-user/src/main/java/com/microservices/apiuser/dto/UserDto.java
@@ -2,6 +2,7 @@ package com.microservices.apiuser.dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +14,12 @@ import lombok.Setter;
 @AllArgsConstructor
 
 public class UserDto {
+	@NotBlank(message = "Nome é obrigatório")
 	private	String	nome;
+	@NotBlank(message = "cpf é obrigatório")
 	private	String	cpf;
 	private	String	endereco;
+	@NotBlank(message = "Email é obrigatório")
 	private	String	email;
 	private	String	telefone;
 	private	LocalDateTime	dataCadastro;
