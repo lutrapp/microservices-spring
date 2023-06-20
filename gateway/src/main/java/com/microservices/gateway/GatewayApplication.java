c70a599324cf89898bd40703b9d81a9964f9e4d1
lutrapp <lutrapp@gmail.com>
2023-06-19 22:55:08 -0300

    add routes in gateway api

--- gateway/src/main/java/com/microservices/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/microservices/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.microservices.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,15 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public	RouteLocator customRouteLocator(RouteLocatorBuilder	builder) {
+		return builder.routes()
+				.route("user_route", r -> r.path("/user/**")
+						.uri("http://localhost:8081"))
+				.route("product_route", r -> r.path("/product/**")
+						.uri("http://localhost:8080"))
+				.route("shopping_route", r -> r.path("/shopping/**")
+						.uri("http://localhost:8082"))
+				.build();
+	}
 }
